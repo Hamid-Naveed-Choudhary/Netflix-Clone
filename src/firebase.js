8074c2dc7e59d93c8f1fc7858ebfcb08d75dedaf
Hamid Naveed <hamidnaveedch@gmail.com>
2024-07-10 02:31:34 +0500

    Implement Login and Registration feature using Firebase

---
+++ src/firebase.js
@@ -0,0 +1,47 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCVj2pg3VadAYUrvWcW0hVtEfQa7mj6h8A",
+    authDomain: "netflix-clone-72be0.firebaseapp.com",
+    projectId: "netflix-clone-72be0",
+    storageBucket: "netflix-clone-72be0.appspot.com",
+    messagingSenderId: "233577626725",
+    appId: "1:233577626725:web:37e8da6fb6d83ae73e1a8c",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc(collection(db, "user"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const login = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const logout = () => {
+    signOut(auth);
+}
+
+export { auth, db, login, signup, logout };
