73199828c056692d8edba50154a80556b110b09e
Hamid Naveed <hamidnaveedch@gmail.com>
2024-07-10 17:20:55 +0500

    Make the website responsive

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -26,38 +26,38 @@ const Login = () => {
   }
 
   return (
-    loading?<div className="login-spinner">
+    loading ? <div className="login-spinner">
       <img src={netflix_spinner} alt="" />
-    </div>:
-    <div className='login'>
-      <img src={logo} className="login-logo" alt='' />
-      <div className="login-form">
-        <h1>{signState}</h1>
-        <form>
-          {signState === "Sign Up" ?
-            <input value={name} onChange={(e) => { setName(e.target.value) }} type="text" placeholder="Yor name" /> : <></>}
+    </div> :
+      <div className='login'>
+        <img src={logo} className="login-logo" alt='' />
+        <div className="login-form">
+          <h1>{signState}</h1>
+          <form>
+            {signState === "Sign Up" ?
+              <input value={name} onChange={(e) => { setName(e.target.value) }} type="text" placeholder="Yor name" /> : <></>}
 
-          <input value={email} onChange={(e) => { setEmail(e.target.value) }} type="email" placeholder="Email" />
-          <input value={password} onChange={(e) => { setPassword(e.target.value) }} type="password" placeholder="Password" />
-          <button onClick={user_auth} type="submit">{signState}</button>
-          <div className="form-help">
-            <div className="remember">
-              <input type="checkbox" />
-              <label htmlFor="">Remember Me</label>
+            <input value={email} onChange={(e) => { setEmail(e.target.value) }} type="email" placeholder="Email" />
+            <input value={password} onChange={(e) => { setPassword(e.target.value) }} type="password" placeholder="Password" />
+            <button onClick={user_auth} type="submit">{signState}</button>
+            <div className="form-help">
+              <div className="remember">
+                <input type="checkbox" />
+                <label htmlFor="">Remember Me</label>
+              </div>
+              <p>Need Help?</p>
             </div>
-            <p>Need Help?</p>
+          </form>
+          <div className="form-switch">
+            {signState === "Sign In" ?
+              <p>New to Netflix? <span onClick={() => { setSignState("Sign Up") }}>Sign Up Now</span></p>
+              : <p>Already have account? <span onClick={() => { setSignState("Sign In") }}>Sign In Now</span></p>
+            }
+
+
           </div>
-        </form>
-        <div className="form-switch">
-          {signState === "Sign In" ?
-            <p>New to Netflix? <span onClick={() => { setSignState("Sign Up") }}>Sign Up Now</span></p>
-            : <p>Already have account? <span onClick={() => { setSignState("Sign In") }}>Sign In Now</span></p>
-          }
-
-
         </div>
       </div>
-    </div>
   )
 }
 
